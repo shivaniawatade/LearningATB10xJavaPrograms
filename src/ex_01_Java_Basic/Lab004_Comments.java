64a4f61cc5024f970794f42fe53b7a3b08698526
shivaniawatade <shivaniawatade25@gmail.com>
2025-01-07 22:44:23 +0530

    initial comment

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+        // this is comment which is ignored by JVM
+        System.out.println("HI, Welcome");
+    }
+}
