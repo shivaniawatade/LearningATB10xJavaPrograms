fb58b0d0fd43367f0345814ca157b4695fc83b45
shivaniawatade <shivaniawatade25@gmail.com>
2025-01-12 00:15:22 +0530

    initial comment

--- src/ex_01_Java_Basic/Lab004_Multiplicationtable9.java
+++ src/ex_01_Java_Basic/Lab004_Multiplicationtable9.java
@@ -3,12 +3,16 @@ package ex_01_Java_Basic;
 public class Lab004_Multiplicationtable9 {
     public static void main(String[] args) {
         int num=9;
-        int a;
-        for(int i=0;i<=num;i++){
-            a=9*i;
-            System.out.printf("9*%d=%d", i,a);
-            System.out.println();
-        }
+        System.out.printf("9*1=%d\n",num*1);
+        System.out.printf("9*2=%d\n",num*2);
+        System.out.printf("9*3=%d\n",num*3);
+        System.out.printf("9*4=%d\n",num*4);
+        System.out.printf("9*5=%d\n",num*5);
+        System.out.printf("9*6=%d\n",num*6);
+        System.out.printf("9*7=%d\n",num*7);
+        System.out.printf("9*8=%d\n",num*8);
+        System.out.printf("9*9=%d\n",num*9);
+        System.out.printf("9*10=%d\n",num*10);
     }
 
 }
