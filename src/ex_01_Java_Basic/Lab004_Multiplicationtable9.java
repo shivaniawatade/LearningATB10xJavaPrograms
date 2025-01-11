1763184a10387a4d667c27a8206046d748726168
shivaniawatade <shivaniawatade25@gmail.com>
2025-01-11 22:30:47 +0530

    initial comment

---
+++ src/ex_01_Java_Basic/Lab004_Multiplicationtable9.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Multiplicationtable9 {
+    public static void main(String[] args) {
+        int num=9;
+        int a;
+        for(int i=0;i<=num;i++){
+            a=9*i;
+            System.out.printf("9*%d=%d", i,a);
+            System.out.println();
+        }
+    }
+
+}
